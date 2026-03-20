eda29b769f6968b9e4115d219a3b785c3611b4f4
Alok Dalke <alokdalke6@gmail.com>
2026-03-20 17:15:08 +0530

    Fix CORS origin configuration in server

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,'https://rbac-three-ochre.vercel.app',
+    origin:'https://rbac-three-ochre.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
